dec413f1449bcca321dffa9c90960a46a738761c
jiyunomegami <github@setq.net>
2024-01-13 22:14:08 +0900

    MPU-401 UART emulation with serial MIDI output

--- sbemu/serial.h
+++ sbemu/serial.h
@@ -2,8 +2,18 @@
 
 #include <stdbool.h>
 
-void
-ser_setup(int sdev);
+#define SBEMU_SERIAL_TYPE_DBG      0
+#define SBEMU_SERIAL_TYPE_MIDI     1
+#define SBEMU_SERIAL_TYPE_FASTDBG  2
+
+int
+ser_setup(int stype, unsigned int sdev);
 
 bool
 ser_puts(const char *s);
+
+void
+ser_putbyte(int b);
+
+void
+ser_print_com_ports();
