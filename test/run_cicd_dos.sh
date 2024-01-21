471c1ed5afd8bf61993bce542bbca0970bf86eb1
Volkert de Buisonjé <volkertb@users.noreply.github.com>
2024-01-21 02:43:27 +0100

    Try emulating ICHx AC'97 instead of Intel HDA, to see if that makes it work

--- test/run_cicd_dos.sh
+++ test/run_cicd_dos.sh
@@ -9,8 +9,7 @@
 -blockdev driver=file,node-name=fd0,filename=/media/x86BOOT.img -device floppy,drive=fd0 \
 -drive if=virtio,format=raw,file=fat:rw:"$(pwd)" \
 -boot order=a \
--device intel-hda \
--audiodev wav,id=snd0,path="$(pwd)"/hda_out.wav -device hda-output,audiodev=snd0 \
+-audiodev wav,id=snd0,path="$(pwd)"/ac97_out.wav -device AC97,audiodev=snd0 \
 -audiodev wav,id=snd1,path="$(pwd)"/virtio_out.wav -device virtio-sound-pci,audiodev=snd1 \
 -audiodev wav,id=snd2,path="$(pwd)"/pcspk_out.wav -machine pcspk-audiodev=snd2 \
 | tee "$(pwd)"/qemu_stdout.log) 3>&1 1>&2 2>&3 | tee "$(pwd)"/qemu_stderr.log
