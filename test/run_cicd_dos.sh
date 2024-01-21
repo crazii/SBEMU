35d1b062886c853a97183a4b7e7120a103a110be
Volkert de Buisonjé <volkertb@users.noreply.github.com>
2024-01-21 01:45:52 +0100

    Replace default QEMU launch script of `cicd-qemu-dos-docker` image with a project-specific one (with different emulated sound devices configured)

---
+++ test/run_cicd_dos.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+(qemu-system-i386 \
+-machine pc,accel=kvm:tcg,hpet=off \
+-smp cpus=1,cores=1 \
+-m 256M \
+-rtc base=localtime \
+-nographic \
+-blockdev driver=file,node-name=fd0,filename=/media/x86BOOT.img -device floppy,drive=fd0 \
+-drive if=virtio,format=raw,file=fat:rw:"$(pwd)" \
+-boot order=a \
+-device ich9-intel-hda \
+-audiodev wav,id=snd0,path="$(pwd)"/hda_out.wav -device hda-output,audiodev=snd0 \
+-audiodev wav,id=snd1,path="$(pwd)"/virtio_out.wav -device virtio-sound-pci,audiodev=snd1 \
+-audiodev wav,id=snd2,path="$(pwd)"/pcspk_out.wav -machine pcspk-audiodev=snd2 \
+| tee "$(pwd)"/qemu_stdout.log) 3>&1 1>&2 2>&3 | tee "$(pwd)"/qemu_stderr.log
