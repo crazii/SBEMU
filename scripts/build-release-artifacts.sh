db8b059b3d89bf9ea51814060f84da4f388c1c0e
crazii <crazii.he@yahoo.com>
2024-04-29 17:28:20 +0800

    pack CHANGELOG.txt to zip.

--- scripts/build-release-artifacts.sh
+++ scripts/build-release-artifacts.sh
@@ -4,6 +4,7 @@ PATH_TO_SBEMU_EXE=${1?param 1 missing - path to SBEMU.EXE}
 test -f "$PATH_TO_SBEMU_EXE" || (echo "File $PATH_TO_SBEMU_EXE does not exit"; exit 1)
 FULL_PATH_TO_SBEMU_EXE=$(readlink -f "$PATH_TO_SBEMU_EXE")
 FULL_PATH_TO_README_TXT=${FULL_PATH_TO_SBEMU_EXE%/*}"/README.txt"
+FULL_PATH_TO_CHANGELOG_TXT=${FULL_PATH_TO_SBEMU_EXE%/*}"/CHANGELOG.txt"
 PATH_TO_OUTPUT_ARTIFACTS=${2?param 2 missing - path to output directory}
 test -d "$PATH_TO_OUTPUT_ARTIFACTS" || (echo "Directory $PATH_TO_OUTPUT_ARTIFACTS does not exit"; exit 1)
 FULL_PATH_TO_OUTPUT_ARTIFACTS=$(readlink -f "$PATH_TO_OUTPUT_ARTIFACTS")
@@ -29,6 +30,7 @@ mkdir /tmp/mnt/sbemu
 cp "$FULL_PATH_TO_SBEMU_EXE" /tmp/mnt/sbemu
 cp "$FULL_PATH_TO_SBEMU_EXE" /tmp/SBEMU
 cp "$FULL_PATH_TO_README_TXT" /tmp/SBEMU
+cp "$FULL_PATH_TO_CHANGELOG_TXT" /tmp/SBEMU
 pushd /tmp/mnt
 mkdir jemm
 cd jemm
