c62d169b5c01d2ca59eddca0bf7ce32e9ecb075e
crazii <crazii.he@yahoo.com>
2026-06-25 17:26:14 +0800

    lowercase: ALSA to alsa.

--- init-ALSA.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-git clone --depth 1 --filter=blob:none --sparse --no-checkout https://github.com/torvalds/linux.git ALSA
-cd ALSA
-git sparse-checkout init --no-cone
-git sparse-checkout set --stdin < ../sparse-checkout-ALSA
-git checkout
