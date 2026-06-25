0b835702208e823e3de8f451d209591e256c88f9
crazii <crazii.he@yahoo.com>
2026-06-25 19:20:15 +0800

    chore: comments.

--- init-alsa.sh
+++ init-alsa.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#if errors occor on clone, update git will solve the problem.
 git clone --depth 1 --filter=blob:none --no-checkout --sparse https://github.com/torvalds/linux.git alsa && \
 cd alsa && \
 git sparse-checkout init --no-cone && \
