dd617dc1909a5777f7938f1566cb9a3d92baa3c0
crazii <crazii.he@yahoo.com>
2026-06-25 18:54:36 +0800

    fix: bugfix on init-alsa.sh

--- init-alsa.sh
+++ init-alsa.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-git clone --depth 1 --filter=blob:none --sparse --no-checkout https://github.com/torvalds/linux.git alsa
-cd alsa
-git sparse-checkout init --no-cone
-git sparse-checkout set --stdin < ../sparse-checkout-alsa
+git clone --depth 1 --filter=blob:none --no-checkout --sparse https://github.com/torvalds/linux.git alsa && \
+cd alsa && \
+git sparse-checkout init --no-cone && \
+git sparse-checkout set --stdin < ../sparse-checkout-alsa && \
 git checkout
